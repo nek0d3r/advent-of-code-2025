61d06955a7497c409406c524351e8d1d45ac5bff
nek0d3r <nek0d3r@gmail.com>
2025-12-11 15:07:59 -0500

    i don't knowwwwww

--- day01/solve.cs
+++ day01/solve.cs
@@ -28,32 +28,62 @@ class Program
         return rotations;
     }
 
-    static void Part1(string path)
+    static void Part1(string path, bool debug = false)
     {
         int position = 50;
         int zeroes = 0;
 
-        Console.WriteLine($"The dial starts by pointing at {position}.");
+        if (debug) Console.WriteLine($"The dial starts by pointing at {position}");
         List<Rotation> rotations = ReadFile(path);
         foreach (Rotation rotation in rotations)
         {
             if (rotation.direction == 'L') position -= rotation.value;
             if (rotation.direction == 'R') position += rotation.value;
+
             while (position < 0) position += 100;
             position %= 100;
 
-            Console.WriteLine($"The dial is rotated {rotation.direction}{rotation.value} to point at {position}");
-            if (position == 0)
-            {
-                zeroes++;
-            }
+            if (debug) Console.WriteLine($"The dial is rotated {rotation.direction}{rotation.value} to point at {position}");
+            if (position == 0) zeroes++;
         }
 
         Console.WriteLine($"The password is {zeroes} for part 1");
     }
 
+    static void Part2(string path, bool debug = false)
+    {
+        int position = 50;
+        int prevPosition = 50;
+        int zeroes = 0;
+
+        if (debug) Console.WriteLine($"The dial starts by pointing at {position}");
+        List<Rotation> rotations = ReadFile(path);
+        foreach (Rotation rotation in rotations)
+        {
+            int clicked = 0;
+            
+            prevPosition = position;
+            if (rotation.direction == 'L') position -= rotation.value;
+            if (rotation.direction == 'R') position += rotation.value;
+
+            if (prevPosition != 0 && (position < 0 || position > 100)) {
+                clicked += Convert.ToInt32(Math.Abs(Math.Floor(position / 100f)));
+                zeroes += clicked;
+            }
+            while (position < 0) position += 100;
+            position %= 100;
+            if (position == 0) zeroes++;
+
+            if (debug) Console.Write($"The dial is rotated {rotation.direction}{rotation.value} to point at {position}");
+            if (debug && clicked > 0) Console.Write($"; during this rotation, it points at zero {clicked} time(s).");
+            if (debug) Console.WriteLine();
+        }
+
+        Console.WriteLine($"The password is {zeroes} for part 2");
+    }
+
     static void Main()
     {
-        Part1("example.txt");
+        Part2("input.txt", true);
     }
 }
