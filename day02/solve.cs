6597b523dffd8af9ea794f3341ecd7bb53e48a6c
nek0d3r <nek0d3r@gmail.com>
2025-12-13 23:50:00 -0500

    solved day 2 part 1

--- day02/solve.cs
+++ day02/solve.cs
@@ -21,16 +21,49 @@ class Program
                 range.Split('-')[1]
             ));
         }
-        foreach(var test in ranges)
-        {
-            Console.WriteLine($"{test.Item1}-{test.Item2}");
-        }
 
         return ranges;
     }
 
+    static void Part1(string path, bool debug = false)
+    {
+        List<Tuple<string, string>> ranges = ReadFile(path);
+
+        long invalid = 0;
+
+        foreach (Tuple<string, string> range in ranges)
+        {
+            var places = new List<int>();
+
+            var num1 = Convert.ToInt64(range.Item1);
+            var num2 = Convert.ToInt64(range.Item2);
+
+            for (var i = range.Item1.Length; i <= range.Item2.Length; i++)
+                if (i % 2 == 0) places.Add(i);
+            if (places.Count == 0) continue;
+
+            bool lowerBoundValid = range.Item1.Count() == places.First();
+            var half1 = lowerBoundValid ?
+                Convert.ToInt64(range.Item1[0..(range.Item1.Length / 2)]) :
+                Convert.ToInt64("1" + new String('0', places[0] / 2 - 1));
+
+            bool upperBoundValid = range.Item2.Count() == places.Last();
+            var half2 = upperBoundValid ?
+                Convert.ToInt64(range.Item2[0..(range.Item2.Length / 2)]) :
+                Convert.ToInt64(new String('9', places[0] / 2));
+
+            for (var i = half1; i <= half2; i++)
+            {
+                var sequence = Convert.ToInt64($"{i}{i}");
+                if (sequence >= num1 && sequence <= num2) invalid += sequence;
+            }
+        }
+
+        Console.WriteLine($"Adding up all the invalid IDs in this input produces {invalid}");
+    }
+
     static void Main()
     {
-        ReadFile("example.txt");
+        Part1("input.txt", true);
     }
 }
