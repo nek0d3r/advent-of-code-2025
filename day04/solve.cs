0daa3478c173b495866b134e85ac12b868d209c9
nek0d3r <nek0d3r@gmail.com>
2025-12-15 03:49:10 -0500

    day 4 part 2 solved

--- day04/solve.cs
+++ day04/solve.cs
@@ -48,8 +48,53 @@ class Program
         Console.WriteLine($"In part 1 there are {rolls} rolls of paper that can be accessed by a forklift");
     }
 
+    static void Part2(string path, bool debug = false)
+    {
+        var rolls = 0;
+        List<Tuple<int, int>> accessible;
+        List<List<string>> map = ReadFile(path);
+
+        do
+        {
+            accessible = new List<Tuple<int, int>>();
+
+            for (var row = 0; row < map.Count(); row++)
+            {
+                for (var col = 0; col < map[row].Count(); col++)
+                {
+                    if (map[row][col] != "@") continue;
+
+                    var adjacent = 0;
+                    if (row > 0)
+                    {
+                        if (col > 0 && map[row - 1][col - 1] == "@") adjacent++;
+                        if (map[row - 1][col] == "@") adjacent++;
+                        if (col < map[row].Count() - 1 && map[row - 1][col + 1] == "@") adjacent++;
+                    }
+                    if (col > 0 && map[row][col - 1] == "@") adjacent++;
+                    if (col < map[row].Count() - 1 && map[row][col + 1] == "@") adjacent++;
+                    if (row < map.Count() - 1)
+                    {
+                        if (col > 0 && map[row + 1][col - 1] == "@") adjacent++;
+                        if (map[row + 1][col] == "@") adjacent++;
+                        if (col < map[row].Count() - 1 && map[row + 1][col + 1] == "@") adjacent++;
+                    }
+
+                    if (adjacent < 4) accessible.Add(new Tuple<int, int>(row, col));
+                }
+            }
+
+            rolls += accessible.Count();
+            foreach (Tuple<int, int> roll in accessible)
+                map[roll.Item1][roll.Item2] = ".";
+        }
+        while (accessible.Count() > 0);
+
+        Console.WriteLine($"In part 2 there are {rolls} rolls of paper that can be accessed by a forklift");
+    }
+
     static void Main()
     {
-        Part1("input.txt", true);
+        Part2("input.txt", true);
     }
 }
